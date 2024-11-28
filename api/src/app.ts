b7a4e0b70de60c6ca3a035cb3e506ae890cffbfe
DiegoRodes <utm23090685@utma.edu.mx>
2024-11-28 10:54:34 -0600

    subiendo archivo

---
+++ api/src/app.ts
@@ -0,0 +1,19 @@
+import express, {Application, Request, Response} from "express"
+import cors from "cors"
+import UsersController from "./controllers/UsersController";
+
+const app: Application = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.get("/",(_req:Request,res:Response)=>{
+    res.send("Hola desde mi servidor con TS")
+})
+
+//Rutas de usuario
+app.post("/user/create", UsersController.signUp)
+app.post("/user/sign-in", UsersController.signIn)
+
+export default app;
